8fc6db9bcc6f38beac8e800b413c03ab91c14e1d
371718330@qq.com <199310qqmm>
2017-05-22 13:07:55 +0800

    新增HttpURLConnection和Webservice网络工具类以及金额计算类

--- CommonUtil/src/main/java/com/jingewenku/abrahamcaijin/commonutil/AppBigDecimal.java
+++ CommonUtil/src/main/java/com/jingewenku/abrahamcaijin/commonutil/AppBigDecimal.java
@@ -31,9 +31,40 @@ public class AppBigDecimal {
 	        BigDecimal b1 = new BigDecimal(Double.toString(v1));  
 	        BigDecimal b2 = new BigDecimal(Double.toString(v2));  
 	        return b1.add(b2).doubleValue();  
-	    }  
-	      
-	    /** 
+	    }
+
+		/**
+		 * 提供精确的加法运算
+		 *
+		 * @param v1 被加数
+		 * @param v2 加数
+		 * @return 两个参数的和
+		 */
+		public static BigDecimal add(String v1, String v2) {
+			BigDecimal b1 = new BigDecimal(v1);
+			BigDecimal b2 = new BigDecimal(v2);
+			return b1.add(b2);
+		}
+
+    /**
+     * 提供精确的加法运算
+     *
+     * @param v1    被加数
+     * @param v2    加数
+     * @param scale 保留scale 位小数
+     * @return 两个参数的和
+     */
+    public static String add(String v1, String v2, int scale) {
+        if (scale < 0) {
+            throw new IllegalArgumentException(
+                "The scale must be a positive integer or zero");
+        }
+        BigDecimal b1 = new BigDecimal(v1);
+        BigDecimal b2 = new BigDecimal(v2);
+        return b1.add(b2).setScale(scale, BigDecimal.ROUND_HALF_UP).toString();
+    }
+
+	/**
 	     * 提供精确的减法运算  
 	     * @param v1 被减数 
 	     * @param v2 减数 
@@ -43,9 +74,40 @@ public class AppBigDecimal {
 	        BigDecimal b1 = new BigDecimal(Double.toString(v1));  
 	        BigDecimal b2 = new BigDecimal(Double.toString(v2));  
 	        return b1.subtract(b2).doubleValue();  
-	    }  
-	      
-	    /** 
+	    }
+
+		/**
+		 * 提供精确的减法运算。
+		 *
+		 * @param v1 被减数
+		 * @param v2 减数
+		 * @return 两个参数的差
+		 */
+		public static BigDecimal sub(String v1, String v2) {
+			BigDecimal b1 = new BigDecimal(v1);
+			BigDecimal b2 = new BigDecimal(v2);
+			return b1.subtract(b2);
+		}
+
+    /**
+     * 提供精确的减法运算
+     *
+     * @param v1    被减数
+     * @param v2    减数
+     * @param scale 保留scale 位小数
+     * @return 两个参数的差
+     */
+    public static String sub(String v1, String v2, int scale) {
+        if (scale < 0) {
+            throw new IllegalArgumentException(
+                "The scale must be a positive integer or zero");
+        }
+        BigDecimal b1 = new BigDecimal(v1);
+        BigDecimal b2 = new BigDecimal(v2);
+        return b1.subtract(b2).setScale(scale, BigDecimal.ROUND_HALF_UP).toString();
+    }
+
+		/**
 	     * 提供精确的乘法运算 
 	     * @param v1 被乘数 
 	     * @param v2 乘数 
@@ -55,8 +117,40 @@ public class AppBigDecimal {
 	        BigDecimal b1 = new BigDecimal(Double.toString(v1));  
 	        BigDecimal b2 = new BigDecimal(Double.toString(v2));  
 	        return b1.multiply(b2).doubleValue();  
-	    }  
-	  
+	    }
+
+    /**
+     * 提供精确的乘法运算
+     *
+     * @param v1    被乘数
+     * @param v2    乘数
+     * @param scale 保留scale 位小数
+     * @return 两个参数的积
+     */
+    public static double mul(double v1, double v2, int scale) {
+        BigDecimal b1 = new BigDecimal(Double.toString(v1));
+        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        return round(b1.multiply(b2).doubleValue(), scale);
+    }
+
+    /**
+     * 提供精确的乘法运算
+     *
+     * @param v1    被乘数
+     * @param v2    乘数
+     * @param scale 保留scale 位小数
+     * @return 两个参数的积
+     */
+    public static String mul(String v1, String v2, int scale) {
+        if (scale < 0) {
+            throw new IllegalArgumentException(
+                "The scale must be a positive integer or zero");
+        }
+        BigDecimal b1 = new BigDecimal(v1);
+        BigDecimal b2 = new BigDecimal(v2);
+        return b1.multiply(b2).setScale(scale, BigDecimal.ROUND_HALF_UP).toString();
+    }
+
 	    /** 
 	     * 提供（相对）精确的除法运算,当发生除不尽的情况时, 
 	     * 精确到小数点以后10位,以后的数字四舍五入. 
@@ -67,7 +161,8 @@ public class AppBigDecimal {
 	    public static double divide(double v1, double v2) {  
 	        return divide(v1, v2, DEF_DIV_SCALE);  
 	    }  
-	  
+
+
 	    /** 
 	     * 提供（相对）精确的除法运算. 
 	     * 当发生除不尽的情况时,由scale参数指 定精度,以后的数字四舍五入. 
@@ -85,7 +180,25 @@ public class AppBigDecimal {
 	        BigDecimal b1 = new BigDecimal(Double.toString(v1));  
 	        BigDecimal b2 = new BigDecimal(Double.toString(v2));  
 	        return b1.divide(b2, scale, BigDecimal.ROUND_HALF_UP).doubleValue();  
-	    }  
+	    }
+
+    /**
+     * 提供（相对）精确的除法运算。当发生除不尽的情况时，由scale参数指
+     * 定精度，以后的数字四舍五入
+     *
+     * @param v1    被除数
+     * @param v2    除数
+     * @param scale 表示需要精确到小数点以后几位
+     * @return 两个参数的商
+     */
+    public static String divide(String v1, String v2, int scale) {
+        if (scale < 0) {
+            throw new IllegalArgumentException("The scale must be a positive integer or zero");
+        }
+        BigDecimal b1 = new BigDecimal(v1);
+        BigDecimal b2 = new BigDecimal(v1);
+        return b1.divide(b2, scale, BigDecimal.ROUND_HALF_UP).toString();
+    }
 	  
 	    /** 
 	     * 提供精确的小数位四舍五入处理 
@@ -101,10 +214,60 @@ public class AppBigDecimal {
 	        BigDecimal b = new BigDecimal(Double.toString(v));  
 	        BigDecimal one = new BigDecimal("1");  
 	        return b.divide(one, scale, BigDecimal.ROUND_HALF_UP).doubleValue();  
-	    }  
-	  
-	    
-	    /**
+	    }
+
+    /**
+     * 提供精确的小数位四舍五入处理
+     *
+     * @param v     需要四舍五入的数字
+     * @param scale 小数点后保留几位
+     * @return 四舍五入后的结果
+     */
+    public static String round(String v, int scale) {
+        if (scale < 0) {
+            throw new IllegalArgumentException(
+                "The scale must be a positive integer or zero");
+        }
+        BigDecimal b = new BigDecimal(v);
+        return b.setScale(scale, BigDecimal.ROUND_HALF_UP).toString();
+    }
+
+    /**
+     * 取余数
+     *
+     * @param v1    被除数
+     * @param v2    除数
+     * @param scale 小数点后保留几位
+     * @return 余数
+     */
+    public static String remainder(String v1, String v2, int scale) {
+        if (scale < 0) {
+            throw new IllegalArgumentException(
+                "The scale must be a positive integer or zero");
+        }
+        BigDecimal b1 = new BigDecimal(v1);
+        BigDecimal b2 = new BigDecimal(v2);
+        return b1.remainder(b2).setScale(scale, BigDecimal.ROUND_HALF_UP).toString();
+    }
+
+    /**
+     * 取余数  BigDecimal
+     *
+     * @param v1    被除数
+     * @param v2    除数
+     * @param scale 小数点后保留几位
+     * @return 余数
+     */
+    public static BigDecimal remainder(BigDecimal v1, BigDecimal v2, int scale) {
+        if (scale < 0) {
+            throw new IllegalArgumentException(
+                "The scale must be a positive integer or zero");
+        }
+        return v1.remainder(v2).setScale(scale, BigDecimal.ROUND_HALF_UP);
+    }
+
+
+    /**
 	     * 金额分割，四舍五人金额
 	     * @param s
 	     * @return
---
+++ CommonUtil/src/main/java/com/jingewenku/abrahamcaijin/commonutil/WebServiceUtils.java
@@ -0,0 +1,119 @@
+package com.jingewenku.abrahamcaijin.commonutil;
+
+import android.os.Handler;
+import android.os.Message;
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpResponseException;
+import org.ksoap2.transport.HttpTransportSE;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @Description:主要功能:WebService工具类
+ * @Prject: CommonUtilLibrary
+ * @Package: com.jingewenku.abrahamcaijin.commonutil
+ * @author: AbrahamCaiJin
+ * @date: 2017年05月22日 11:47
+ * @Copyright: 个人版权所有
+ * @Company:
+ * @version: 1.0.0
+ */
+
+public class WebServiceUtils {
+    public static final String WEB_SERVER_URL = "";
+    // 含有3个线程的线程池
+    private static final ExecutorService executorService = Executors
+        .newFixedThreadPool(3);
+    // 命名空间
+    private static final String NAMESPACE = "";
+
+
+    /**
+     *
+     * @param url
+     *            WebService服务器地址
+     * @param methodName
+     *            WebService的调用方法名
+     * @param properties
+     *            WebService的参数
+     * @param webServiceCallBack
+     *            回调接口
+     */
+    public static void callWebService(String url, final String methodName,
+        HashMap<String, String> properties,
+        final WebServiceCallBack webServiceCallBack) {
+        // 创建HttpTransportSE对象，传递WebService服务器地址
+        final HttpTransportSE httpTransportSE = new HttpTransportSE(url);
+        // 创建SoapObject对象
+        SoapObject soapObject = new SoapObject(NAMESPACE, methodName);
+
+        // SoapObject添加参数
+
+        //写真实的变量名也没有用，传递参数必须是arg开头，必须是arg0 ,arg1 一直拍下去
+
+        if (properties != null) {
+            for (Iterator<Entry<String, String>> it = properties.entrySet()
+                                                                .iterator(); it.hasNext();) {
+                Map.Entry<String, String> entry = it.next();
+                soapObject.addProperty(entry.getKey(), entry.getValue());
+            }
+        }
+        // 实例化SoapSerializationEnvelope，传入WebService的SOAP协议的版本号
+
+        //tomcat 7.055 for 64  jdk 1.6 for 64  web3.0 这里写 ver12 ，如果是ver11会报 http 415错误
+        final SoapSerializationEnvelope soapEnvelope = new SoapSerializationEnvelope(
+            SoapEnvelope.VER12);
+        // 设置是否调用的是.Net开发的WebService
+        soapEnvelope.bodyOut = soapObject;
+        soapEnvelope.dotNet = false;//如果调用的是.Net的WebService，这里为true，否则为false，不然调用会不成功
+        soapEnvelope.setOutputSoapObject(soapObject);
+        httpTransportSE.debug = true;
+        // 用于子线程与主线程通信的Handler
+        final Handler mHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                // 将返回值回调到callBack的参数中
+                webServiceCallBack.callBack((Object) msg.obj);
+            }
+        };
+
+        // 开启线程去访问WebService
+        executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                Object resultSoapObject = null;
+                try {
+                    httpTransportSE.call(WEB_SERVER_URL + methodName, soapEnvelope);
+                    if (soapEnvelope.getResponse() != null) {
+                        // 获取服务器响应返回的SoapObject
+                        resultSoapObject = (Object) soapEnvelope.bodyOut;
+                    }
+                } catch (HttpResponseException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (XmlPullParserException e) {
+                    e.printStackTrace();
+                } finally {
+                    // 将获取的消息利用Handler发送到主线程
+                    mHandler.sendMessage(mHandler.obtainMessage(0,
+                        resultSoapObject));
+                }
+            }
+        });
+    }
+
+    public interface WebServiceCallBack {
+        public void callBack(Object result);
+    }
+}
