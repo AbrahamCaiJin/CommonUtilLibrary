825eaa873472627c2b327964442563cd14e095ac
371718330@qq.com <199310qqmm>
2017-06-01 15:39:41 +0800

    增加正则匹配

--- CommonUtil/src/main/java/com/jingewenku/abrahamcaijin/commonutil/AppValidationMgr.java
+++ CommonUtil/src/main/java/com/jingewenku/abrahamcaijin/commonutil/AppValidationMgr.java
@@ -460,7 +460,66 @@ public class AppValidationMgr {
 		return username_pattern.matcher(username).matches();
 	}
 
-	
+	/**
+	 * 手机号码，中间4位星号替换
+	 *
+	 * @param phone 手机号
+	 * @return 星号替换的手机号
+	 */
+	public static String phoneNoHide(String phone) {
+		// 括号表示组，被替换的部分$n表示第n组的内容
+		// 正则表达式中，替换字符串，括号的意思是分组，在replace()方法中，
+		// 参数二中可以使用$n(n为数字)来依次引用模式串中用括号定义的字串。
+		// "(\d{3})\d{4}(\d{4})", "$1****$2"的这个意思就是用括号，
+		// 分为(前3个数字)中间4个数字(最后4个数字)替换为(第一组数值，保持不变$1)(中间为*)(第二组数值，保持不变$2)
+		return phone.replaceAll("(\\d{3})\\d{4}(\\d{4})", "$1****$2");
+	}
+
+	/**
+	 * 银行卡号，保留最后4位，其他星号替换
+	 *
+	 * @param cardId 卡号
+	 * @return 星号替换的银行卡号
+	 */
+	public static String cardIdHide(String cardId) {
+		return cardId.replaceAll("\\d{15}(\\d{3})", "**** **** **** **** $1");
+	}
+
+	/**
+	 * 身份证号，中间10位星号替换
+	 *
+	 * @param id 身份证号
+	 * @return 星号替换的身份证号
+	 */
+	public static String idHide(String id) {
+		return id.replaceAll("(\\d{4})\\d{10}(\\d{4})", "$1** **** ****$2");
+	}
+
+	/**
+	 * 是否为车牌号（沪A88888）
+	 *
+	 * @param vehicleNo 车牌号
+	 * @return 是否为车牌号
+	 */
+
+	public static boolean checkVehicleNo(String vehicleNo) {
+		Pattern pattern = Pattern.compile("^[\u4e00-\u9fa5]{1}[a-zA-Z]{1}[a-zA-Z_0-9]{5}$");
+		return pattern.matcher(vehicleNo).find();
+
+	}
+
+//	/**
+//	 * 匹配中国邮政编码
+//	 *
+//	 * @param postcode 邮政编码
+//	 * @return 验证成功返回true，验证失败返回false
+//	 */
+//	public static boolean checkPostcode(String postcode) {
+//		String regex = "[1-9]\\d{5}";
+//		return Pattern.matches(regex, postcode);
+//	}
+
+
 	
 	
 
