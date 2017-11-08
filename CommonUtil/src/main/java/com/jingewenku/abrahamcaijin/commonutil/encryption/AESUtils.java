03d3606e2cfdf2c5e9374cda097f90a5911ca870
371718330@qq.com <199310qqmm>
2017-11-08 10:02:30 +0800

    修改AESUtils

--- CommonUtil/src/main/java/com/jingewenku/abrahamcaijin/commonutil/encryption/AESUtils.java
+++ CommonUtil/src/main/java/com/jingewenku/abrahamcaijin/commonutil/encryption/AESUtils.java
@@ -1,9 +1,12 @@
 package com.jingewenku.abrahamcaijin.commonutil.encryption;
 
+import android.util.Base64;
+
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 /**
@@ -19,9 +22,6 @@ import java.security.NoSuchAlgorithmException;
 
 public class AESUtils {
 
-    private AESUtils() {
-        throw new UnsupportedOperationException("cannot be instantiated");
-    }
 
     /*
      * 生成密钥
@@ -55,6 +55,78 @@ public class AESUtils {
         cipher.init(Cipher.DECRYPT_MODE, secretKey);
         byte[] plainBytes = cipher.doFinal(data);
         return plainBytes;
+    }
+
+
+    private SecretKeySpec skeySpec;
+    private Cipher cipher;
+
+    public AESUtils(byte[] keyraw) throws Exception {
+        if (keyraw == null) {
+            byte[] bytesOfMessage = "".getBytes("UTF-8");
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] bytes = md.digest(bytesOfMessage);
+
+            skeySpec = new SecretKeySpec(bytes, "AES");
+            cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        } else {
+
+            skeySpec = new SecretKeySpec(keyraw, "AES");
+            cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+
+        }
+    }
+
+    public AESUtils(String passphrase) throws Exception {
+        byte[] bytesOfMessage = passphrase.getBytes("UTF-8");
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] thedigest = md.digest(bytesOfMessage);
+        skeySpec = new SecretKeySpec(thedigest, "AES");
+
+        cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+    }
+
+    public AESUtils() throws Exception {
+        byte[] bytesOfMessage = "".getBytes("UTF-8");
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] thedigest = md.digest(bytesOfMessage);
+        skeySpec = new SecretKeySpec(thedigest, "AES");
+
+        skeySpec = new SecretKeySpec(new byte[16], "AES");
+        cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+    }
+
+    /**
+     * 加密:先AES再base64
+     *
+     * @param plaintext
+     * @return
+     * @throws Exception
+     */
+    public String encrypt(String plaintext) throws Exception {
+
+        cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+
+        byte[] ciphertext = cipher.doFinal(plaintext.getBytes("UTF-8"));
+
+        return Base64.encodeToString(ciphertext, Base64.DEFAULT);
+    }
+
+    /**
+     * 解密:先base64再AES
+     *
+     * @param ciphertext
+     * @return
+     * @throws Exception
+     */
+    public String decrypt(String ciphertext) throws Exception {
+        cipher.init(Cipher.DECRYPT_MODE, skeySpec);
+
+        byte[] plain64text = Base64.decode(ciphertext, Base64.DEFAULT);
+
+        byte[] plaintext = cipher.doFinal(plain64text);
+
+        return new String(plaintext, "UTF-8");
     }
 
 }
