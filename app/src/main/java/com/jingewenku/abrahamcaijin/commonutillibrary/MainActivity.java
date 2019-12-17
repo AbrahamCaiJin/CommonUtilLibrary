9d1a46c7654d1994b2592159326cae68e9f9aa33
AbrahamCaiJin <1652783461@qq.com>
2019-12-17 15:42:00 +0800

    更新版本库和支持库，增加新工具类，去除权限引用，改为使用者自己添加所需权限

--- app/src/main/java/com/jingewenku/abrahamcaijin/commonutillibrary/MainActivity.java
+++ app/src/main/java/com/jingewenku/abrahamcaijin/commonutillibrary/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        showTxt = (TextView) findViewById(R.id.show_txt);
+        showTxt = findViewById(R.id.show_txt);
         showTxt.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
