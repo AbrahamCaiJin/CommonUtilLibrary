51c76682aa469dbaa6315dfb966c5b3dd8b729af
AbrahamCaiJin <1652783461@qq.com>
2020-01-09 16:09:22 +0800

    增加CommonUtilLibraryApplication

---
+++ app/src/main/java/com/jingewenku/abrahamcaijin/commonutillibrary/application/CommonUtilLibraryApplication.java
@@ -0,0 +1,22 @@
+package com.jingewenku.abrahamcaijin.commonutillibrary.application;
+
+import android.app.Application;
+
+/**
+ * 主要功能:
+ *
+ * @Description:
+ * @Project: CommonUtilLibrary
+ * @Package: com.jingewenku.abrahamcaijin.commonutillibrary.application
+ * @author: Abraham
+ * @Email:
+ * @date: 2020年01月09日 16:00
+ * @Copyright: 个人版权所有
+ * @Company:
+ * @version: 1.0.0
+ * @Modify date and time:
+ * @Modified by:
+ * @Modified remark:
+ */
+public class CommonUtilLibraryApplication extends Application {
+}
